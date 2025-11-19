fcf5fc32991b596df7b1899c750172730578c84a
carlosPc1987 <cadecozarrus@gmail.com>
2025-11-19 11:38:11 +0100

    changes to blackjack

--- src/main/java/cat/itacademy/s05/t01/n01/blackjack/BlackjackApplication.java
+++ src/main/java/cat/itacademy/s05/t01/n01/blackjack/BlackjackApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = "cat.itacademy.s05.t01.n01.S05T01N01.domain.sql")
-public class BlackjackApplication {
+public class  BlackjackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlackjackApplication.class, args);
 	}
---
+++ src/main/java/cat/itacademy/s05/t01/n01/blackjack/dto/response/PlayerResponse.java
@@ -0,0 +1,4 @@
+package cat.itacademy.s05.t01.n01.blackjack.dto.response;
+
+public class PlayerResponse {
+}
